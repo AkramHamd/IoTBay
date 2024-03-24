77b8a76e9e6d014c5f80d89b43178bae776da10c
Ahmad Yawari <k_han21@yahoo.com>
2024-03-24 20:30:31 +1100

    initial project setup

---
+++ src/main/java/uts/isd/model/Calculator.java
@@ -0,0 +1,29 @@
+package uts.isd.model;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public double power(int a, int b) {
+        return Math.pow(a, b);
+    }
+
+    public int evaluate(String expression) {
+        int sum = 0;
+
+        for (String summand : expression.split("\\+")) {
+            sum += Integer.valueOf(summand);
+        }
+
+        return sum;
+    }
+}
