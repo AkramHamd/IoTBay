12a90f77dc483b0a14217a85d3427cf7538095a9
BMaggi-UTS <bradley.madgwick@student.uts.edu.au>
2024-05-01 20:30:08 +1000

    my bad fixing this file

--- src/main/java/uts/isd/model/Product.java
+++ src/main/java/uts/isd/model/Product.java
@@ -1,41 +1,6 @@
 package uts.isd.model;
 import java.io.Serializable;
 
-
-public class Product implements Serializable {
-    private String productName;
-    private String productBrand;
-    private String productDescription;
-    private String productImage;
-    private Integer productPrice;
-    private Integer specialPrice;
-    private boolean productOnSpecial;
-    private Integer productStock;
-    private Integer orderQty;
-
-    public Product(
-        String productName,
-        String productBrand,
-        String productDescription,
-        String productImage,
-        Integer productPrice,
-        Integer specialPrice,
-        Boolean productOnSpecial,
-        Integer productStock,
-        Integer orderQty) {
-        this.productName = productName;
-        this.productBrand = productBrand;
-        this.productDescription = productDescription;
-        this.productImage = productImage;
-        this.productPrice = productPrice;
-        this.specialPrice = specialPrice;
-        this.productOnSpecial = productOnSpecial;
-        this.productStock = productStock;
-        }
-        
-    public String getProductName() {
-        return this.productName;
-
 public class Product implements Serializable{
     
     private int productId;
@@ -65,7 +30,7 @@ public class Product implements Serializable{
     }
 
     public int getProductId() {
-        return productId;
+        return this.productId;
     }
 
     public void setProductId(int productId) {
@@ -73,7 +38,7 @@ public class Product implements Serializable{
     }
 
     public String getProductName() {
-        return productName;
+        return this.productName;
     }
 
     public void setProductName(String productName) {
@@ -96,29 +61,28 @@ public class Product implements Serializable{
         this.productDescription = productDescription;
     }
 
-
-    public String getProductImage() {
-        return this.productImage;
+    public String getProductImg() {
+        return this.productImg;
     }
 
-    public void setProductImage(String productImage) {
-        this.productImage = productImage;
+    public void setProductImg(String productImg) {
+        this.productImg = productImg;
     }
 
-    public Integer getProductPrice() {
+    public double getProductPrice() {
         return this.productPrice;
     }
 
-    public void setProductPrice(Integer productPrice) {
+    public void setProductPrice(double productPrice) {
         this.productPrice = productPrice;
     }
 
-    public Integer getSpecialPrice() {
-        return this.specialPrice;
+    public double getProductSpecialPrice() {
+        return this.productSpecialPrice;
     }
 
-    public void setSpecialPrice(Integer specialPrice) {
-        this.specialPrice = specialPrice;
+    public void setProductSpecialPrice(double productSpecialPrice) {
+        this.productSpecialPrice = productSpecialPrice;
     }
 
     public boolean isProductOnSpecial() {
@@ -127,57 +91,14 @@ public class Product implements Serializable{
 
     public boolean getProductOnSpecial() {
         return this.productOnSpecial;
-
-    public String getProductImg() {
-        return productImg;
-    }
-
-    public void setProductImg(String productImg) {
-        this.productImg = productImg;
-    }
-
-    public double getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public double getProductSpecialPrice() {
-        return productSpecialPrice;
-    }
-
-    public void setProductSpecialPrice(double productSpecialPrice) {
-        this.productSpecialPrice = productSpecialPrice;
-    }
-
-    public boolean isProductOnSpecial() {
-        return productOnSpecial;
     }
 
     public void setProductOnSpecial(boolean productOnSpecial) {
         this.productOnSpecial = productOnSpecial;
     }
 
-
-    public Integer getProductStock() {
-        return this.productStock;
-    }
-
-    public void setProductStock(Integer productStock) {
-        this.productStock = productStock;
-    }
-
-    public Integer getOrderQty() {
-        return this.orderQty;
-    }
-
-    public void setOrderQty(Integer orderQty) {
-        this.orderQty = orderQty;
-        
     public int getProductStock() {
-        return productStock;
+        return this.productStock;
     }
 
     public void setProductStock(int productStock) {
@@ -185,10 +106,12 @@ public class Product implements Serializable{
     }
 
     public int getProductOrderQty() {
-        return productOrderQty;
+        return this.productOrderQty;
     }
 
     public void setProductOrderQty(int productOrderQty) {
         this.productOrderQty = productOrderQty;
     }
+
+    
 }
